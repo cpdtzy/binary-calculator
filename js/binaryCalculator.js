dda8f172403e7cc59c4f3e140c8dfa053b5e17a8
cpd <cpdblbq@163.com>
2018-05-31 20:24:33 +0800

    finished

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,45 +1,109 @@
 (function () {
-    const res = document.getElementById('res');
-    const btn0 = document.getElementById('btn0');
-    const btn1 = document.getElementById('btn1');
-    const btnClr = document.getElementById('btnClr');
-    const btnEql = document.getElementById('btnEql');
-    const btnSum = document.getElementById('btnSum');
-    const btnSub = document.getElementById('btnSub');
-    const btnMul = document.getElementById('btnMul');
-    const btnDiv = document.getElementById('btnDiv');
-
-    let [value1, value2] = [0, 0];
-
-    addValue(btn1, 1);
-    addValue(btn0, 0);
-    addSymbol(btnSum, '+');
-    addSymbol(btnSub, '-');
-    addSymbol(btnMul, '*');
-    addSymbol(btnDiv, '/');
-
-    btnClr.onclick = () => {
+    var res = document.getElementById('res');
+    var btnClr = document.getElementById('btnClr');
+    var btnEql = document.getElementById('btnEql');
+    addValue("btn1", 1);
+    addValue("btn0", 0);
+    addSymbol("btnSum", '+');
+    addSymbol("btnSub", '-');
+    addSymbol("btnMul", '*');
+    addSymbol("btnDiv", '/');
+    btnClr.onclick = function () {
         res.innerHTML = '';
-    }
-    
-    function addValue(element, value) {
-        element.onclick = () => {
-
-            res.innerHTML += value;
+    };
+    btnEql.onclick = function () {
+        var value = res.innerHTML;
+        var test = new RegExp('[\+\-\/\*]', 'ig');
+        if (!value) {
+            alert("Please add value.");
         }
+        else if (!test.test(value)) {
+            alert("Please add symbol.");
+        }
+        else {
+            getAnswer(value);
+        }
+        function getAnswer(value) {
+            var sum = /\+/ig;
+            var sub = /\-/ig;
+            var mul = /\*/ig;
+            var div = /\//ig;
+            if (sum.test(value))
+                getFinished(value, ' + ');
+            else if (sub.test(value))
+                getFinished(value, ' - ');
+            else if (mul.test(value))
+                getFinished(value, ' * ');
+            else if (div.test(value))
+                getFinished(value, ' / ');
+            function getFinished(value2, symbol) {
+                var number = value2.split(symbol);
+                var calculation = new BinaryCalculation(number[0], number[1]);
+                var answer;
+                switch (symbol) {
+                    case ' + ':
+                        answer = calculation.sum();
+                        break;
+                    case ' - ':
+                        answer = calculation.sub();
+                        break;
+                    case ' * ':
+                        answer = calculation.mul();
+                        break;
+                    case ' / ':
+                        answer = calculation.div();
+                        break;
+                }
+                res.innerHTML = answer;
+            }
+        }
+    };
+    function addValue(element, value) {
+        var obj = document.getElementById(element);
+        obj.onclick = function () {
+            res.innerHTML += value;
+        };
     }
-
     function addSymbol(element, symbolValue) {
-
-        element.onclick = () => {
-            value1 = res.innerHTML;
-
-            if (!value1) 
+        var obj = document.getElementById(element);
+        obj.onclick = function () {
+            var test = new RegExp('[\+\-\/\*]', 'ig');
+            var value1 = res.innerHTML;
+            if (!value1)
                 alert("Please add value.");
+            else if (test.test(value1))
+                return;
             else {
                 value1 = res.innerHTML;
                 res.innerHTML += ' ' + symbolValue + ' ';
             }
-        }
+        };
     }
+    var BinaryCalculation = (function () {
+        function BinaryCalculation(value, value1) {
+            this.value = value;
+            this.value1 = value1;
+            this.number = parseInt(value, 2);
+            this.number2 = parseInt(value1, 2);
+        }
+        BinaryCalculation.prototype.sum = function () {
+            var sum = this.number + this.number2;
+            return sum.toString(2);
+        };
+        BinaryCalculation.prototype.sub = function () {
+            var sub = this.number - this.number2;
+            return sub.toString(2);
+        };
+        BinaryCalculation.prototype.mul = function () {
+            var mul = this.number * this.number2;
+            return mul.toString(2);
+        };
+        BinaryCalculation.prototype.div = function () {
+            var mul = this.number / this.number2;
+            return mul.toString(2);
+        };
+        return BinaryCalculation;
+    }());
 })();
+
+//# sourceMappingURL=binaryCalculator.js.map
---
+++ ts/binaryCalculator.ts
@@ -0,0 +1,139 @@
+(function () {
+    const res = document.getElementById('res');
+    const btnClr = document.getElementById('btnClr');
+    const btnEql = document.getElementById('btnEql');
+
+    addValue("btn1", 1);
+    addValue("btn0", 0);
+    addSymbol("btnSum", '+');
+    addSymbol("btnSub", '-');
+    addSymbol("btnMul", '*');
+    addSymbol("btnDiv", '/');
+
+    // clear value
+    btnClr.onclick = () => {
+        res.innerHTML = '';
+    }
+
+    btnEql.onclick = () => {
+        const value = res.innerHTML;
+        const test = new RegExp('[\+\-\/\*]', 'ig');
+
+        if ( !value ) {
+            alert("Please add value.");
+
+        } else if( !test.test(value) ) {
+            alert("Please add symbol.");
+
+        } else {
+            getAnswer(value);
+        }
+
+        function getAnswer(value: string): void {
+            const sum = /\+/ig;
+            const sub = /\-/ig;
+            const mul = /\*/ig;
+            const div = /\//ig;
+
+            if (sum.test(value)) 
+                getFinished(value, ' + ');
+
+            else if(sub.test(value))
+                getFinished(value, ' - ');
+
+            else if(mul.test(value))
+                getFinished(value, ' * ');
+            
+            else if(div.test(value))
+                getFinished(value, ' / ');
+
+            function getFinished(value2: string, symbol: string): void {
+                const number: string[] = value2.split(symbol);
+
+                // continue not finish.
+                const calculation = new BinaryCalculation(number[0], number[1]);
+                let answer: string;
+                switch(symbol) {
+                    case ' + ':
+                        answer = calculation.sum(); 
+                    break;
+                    case ' - ':
+                        answer = calculation.sub();
+                    break;
+                    case ' * ':
+                        answer = calculation.mul();
+                    break;
+                    case ' / ':
+                        answer = calculation.div();
+                    break;
+                }
+
+                res.innerHTML = answer;
+            }
+        }
+    }
+    
+    function addValue(element: string, value: number): void {
+        const obj = document.getElementById(element);
+
+        obj.onclick = () => {
+
+            res.innerHTML += value;
+        }
+    }
+
+    function addSymbol(element: string, symbolValue: string): void {
+        const obj = document.getElementById(element);
+
+        obj.onclick = () => {
+            const test = new RegExp('[\+\-\/\*]', 'ig');
+            let value1 = res.innerHTML;
+
+            if (!value1) 
+                alert("Please add value.");
+
+            else if(test.test(value1))
+                return;
+                
+            else {
+                value1 = res.innerHTML;
+                res.innerHTML += ' ' + symbolValue + ' ';
+            }
+        }
+    }
+
+    // binary calculation formula
+    class BinaryCalculation {
+        number: number;
+        number2: number;
+
+        constructor(public value: string, public value1: string) {
+            this.number = parseInt(value, 2);
+            this.number2 = parseInt(value1, 2);
+        }
+
+        public sum() {
+            const sum: number = this.number + this.number2;
+            
+            return sum.toString(2);
+        }
+
+        public sub() {
+            const sub: number = this.number - this.number2;
+
+            return sub.toString(2);
+        }
+
+        public mul() {
+            const mul: number = this.number * this.number2;
+
+            return mul.toString(2);
+        }
+
+        public div() {
+            const mul: number = this.number / this.number2;
+
+            return mul.toString(2);
+        }
+    }
+})();
